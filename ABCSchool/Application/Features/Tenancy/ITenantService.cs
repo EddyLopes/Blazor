4ad4842738d3cba8afecc85dc6f20f0f5f18d27a
EddyLopes <edimar.lopes10@gmail.com>
2026-04-16 06:33:05 -0300

    update

--- ABCSchool/Application/Features/Tenancy/ITenantService.cs
+++ ABCSchool/Application/Features/Tenancy/ITenantService.cs
@@ -5,7 +5,7 @@ public interface ITenantService
     Task<string> CreateTenantAsync(CreateTenantRequest createRequest, CancellationToken ct);
     Task<string> ActivateAsync(string id);
     Task<string> DeactivateAsync(string id);
-    Task<string> UpdateSubscriptionAsync(string id, DateTime newExpireDate);
+    Task<string> UpdateSubscriptionAsync(UpdateTenantSubscriptionRequest updateRequest);
     Task<List<TenantResponse>> GetTenantsAsync();
     Task<TenantResponse> GetTenantByIdAsync(string id);
 }
---
+++ ABCSchool/Application/Features/Tenancy/Queries/GetTenantByIdQuery.cs
@@ -0,0 +1,34 @@
+﻿using Application.Wrappers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.Tenancy.Queries;
+
+public class GetTenantByIdQuery : IRequest<IResponseWrapper>
+{
+    public string TenantId { get; set; }
+}
+
+public class GetTenantByIdQueryHandler : IRequestHandler<GetTenantByIdQuery, IResponseWrapper>
+{
+    private readonly ITenantService _tenantService;
+
+    public GetTenantByIdQueryHandler(ITenantService tenantService)
+    {
+        _tenantService = tenantService;
+    }
+
+    public async Task<IResponseWrapper> Handle(GetTenantByIdQuery request, CancellationToken cancellationToken)
+    {
+        var tenant = await _tenantService.GetTenantByIdAsync(request.TenantId);
+        
+        if(tenant is null)
+        {
+            return await ResponseWrapper<TenantResponse>.FailAsync(message: "Tenant not found");
+        }
+
+        return await ResponseWrapper<TenantResponse>.SuccessAsync(data: tenant, message: "Tenant found successfully.");
+    }
+}
