64d4d119ad0413365489d877269b59e0c05b7fb3
EddyLopes <edimar.lopes10@gmail.com>
2026-05-19 06:05:47 -0300

    update

---
+++ ABCSchool/App.Infrastructure/Extensions/WasmHostBuilderEntensions.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Infrastructure.Extensions;
+
+public static class WasmHostBuilderEntensions
+{
+    public static WebAssemblyHostBuilder AddClientServices(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddAuthorizationCore();
+
+        return builder;
+    }
+
+    private static void RegisterPermissions(AuthorizationOptions options)
+    {
+        foreach (var permission in SchoolPermissions.All)
+        {
+            options.AddPolicy(permission.Name, policy => policy.RequireClaim(ClaimConstants.Permission, permission.Name));
+        }
+    }
+}
---
+++ ABCSchool/App.Infrastructure/Services/Auth/ShouldHavePermissionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Constants;
+using Microsoft.AspNetCore.Authorization;
+
+namespace App.Infrastructure.Services.Auth;
+
+public class ShouldHavePermissionAttribute : AuthorizeAttribute
+{
+    public ShouldHavePermissionAttribute(string action, string feature)
+    {
+        Policy = SchoolPermission.NameFor(action, feature);
+    }
+}
