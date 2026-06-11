7be4531da3a6baa7b1aeecb6c14c57b0ea680987
Edimar Lopes <Edimar@asv.com.br>
2026-06-11 08:19:20 -0300

    update

---
+++ ABCSchool/App.Infrastructure/Services/Identity/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using ABCSharedLibrary.Models.Requests.Identity.Tokens;
+using ABCSharedLibrary.Wrappers;
+
+namespace App.Infrastructure.Services.Identity;
+
+public interface ITokenService
+{
+    Task<IResponseWrapper> LoginAsync(string tenant, TokenRequest request);
+}
---
+++ ABCSchool/App.Infrastructure/Services/Implementations/Identity/TokenService.cs
@@ -0,0 +1,28 @@
+﻿using ABCSharedLibrary.Models.Requests.Identity.Tokens;
+using ABCSharedLibrary.Wrappers;
+using App.Infrastructure.Services.Identity;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace App.Infrastructure.Services.Implementations.Identity;
+
+public class TokenService : ITokenService
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localStorageService;
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
+    private readonly ApiSettings _apiSettings;
+
+    public TokenService(HttpClient httpClient, ILocalStorageService localStorageService, AuthenticationStateProvider authenticationStateProvider, ApiSettings apiSettings)
+    {
+        _httpClient = httpClient;
+        _localStorageService = localStorageService;
+        _authenticationStateProvider = authenticationStateProvider;
+        _apiSettings = apiSettings;
+    }
+
+    public Task<IResponseWrapper> LoginAsync(string tenant, TokenRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
